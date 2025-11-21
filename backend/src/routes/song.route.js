e3c760030614262c7ee8428cf63515bef2d6f8fe
Pascal <mainbuat575@gmail.com>
2025-11-21 21:12:40 +0700

    minor bug fix

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -4,7 +4,7 @@ import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
-router.get('/',protectRoute,getAllSongs)
+router.get('/',getAllSongs)
 router.get('/featured',getFeaturedSongs)
 router.get('/made-for-you',getMadeForYouSongs)
 router.get('/trending',getTrendingSongs)
