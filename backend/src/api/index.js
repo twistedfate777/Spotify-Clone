390d0a067b9a733a691110b9db70608d79aa5afd
Pascal <mainbuat575@gmail.com>
2025-11-23 07:56:54 +0700

    fixed route issue

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -55,9 +55,5 @@ app.use('/api/stats',statsRoutes)
 app.use((err, req, res, next) => {
 	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
 });
-
-
-httpServer.listen(PORT,()=>{
-    connectDB()
-    console.log("Server is running at Port ",+PORT)
-})
+connectDB()
+export default app;
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL:"https://spotify-clone-bice-two.vercel.app//api"
+  baseURL:"https://spotify-clone-bice-two.vercel.app/api"
 })
