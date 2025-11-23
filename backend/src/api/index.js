5f9992096bdc93e11c793ab679f892f7d1fb6c70
Pascal <mainbuat575@gmail.com>
2025-11-23 08:07:57 +0700

    fixed-cors-error

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -28,7 +28,7 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:"https://spotify-clone-binc.vercel.app/",
   credentials:true
 }))
 
